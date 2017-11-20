08ce9c3a1a4640c9c977a5993dee4f05442c2d03
Saltuk Erkan <bugraerkan97@gmail.com>
2017-11-20 21:35:48 +0300

    First Commit

---
+++ Assets/BounceScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceScript : MonoBehaviour {
+
+
+    Vector3 epicentre;
+
+	// Use this for initialization
+	void Start () {
+        epicentre = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject dice = GameObject.FindGameObjectWithTag("Dice");
+        Rigidbody rb = dice.GetComponent<Rigidbody>();
+        rb.AddExplosionForce(700, epicentre, 5, 3);
+    }
+}
---
+++ Assets/FollowDice.cs
@@ -0,0 +1,34 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDice : MonoBehaviour
+{
+    Camera c;
+    GameObject[] Dices;
+    Transform Player1;
+    Transform Player2;
+    Vector3 Offset = new Vector3(0,20,0);
+
+    void Start()
+    {
+        c = GetComponent<Camera>();
+        Dices = GameObject.FindGameObjectsWithTag("Dice");
+        Player1 = Dices[0].transform;
+        Player2 = Dices[1].transform;
+    }
+    void LateUpdate()
+    {
+        if (Player1!= null && Player2 != null)
+        {
+
+            Vector3 tmp = (Player1.position + Player2.position)/2 + Offset;
+
+
+
+
+        }
+        
+        
+    }
+}*/
