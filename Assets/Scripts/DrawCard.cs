0ab12a1170a93a64c1236b3ec5e0c53927e8894a
Saltuk Erkan <bugraerkan97@gmail.com>
2017-12-21 22:41:41 +0300

    Added UI functionality

---
+++ Assets/Scripts/DrawCard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawCard : MonoBehaviour {
+
+    public Image image;
+
+    private void Start()
+    {
+        image.enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        image.enabled = !image.enabled;
+    }
+}
