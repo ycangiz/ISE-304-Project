b41309fb71252f39a7075a6adb0daaddd4955331
Saltuk Erkan <bugraerkan97@gmail.com>
2017-12-23 16:48:35 +0300

    Added network components and some scripts

--- Assets/Scripts/Roll.cs
+++ Assets/Scripts/Roll.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 
 public class Roll : MonoBehaviour {
 
-	// Use this for initialization
+    Rigidbody rb;
+    
+    // Use this for initialization
 	void Start () {
 
-        Rigidbody rb;
-        rb = GetComponent<Rigidbody>();
-        int r = Random.Range(0, 1);
-        if (r == 0)
-        {
-            rb.AddTorque(Random.Range(0, 1000) % 10, Random.Range(0, 1000) % 10, Random.Range(0, 1000) % 10);
-            rb.AddForce(Random.Range(0, 1000) % 10*20, Random.Range(0, 1000) % 10*20, Random.Range(0, 1000) % 10*20);
-        }
-        else
-        {
-            rb.AddForce(-Random.Range(0, 1000) % 10*20, -Random.Range(0, 1000) % 10*20, -Random.Range(0, 1000) % 10*20);
-            rb.AddTorque(-Random.Range(0, 1000) % 10, -Random.Range(0, 1000) % 10, -Random.Range(0, 1000) % 10);
-        }
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void RollDice()
+    {
+        rb = GetComponent<Rigidbody>();
+        int r = Random.Range(0, 1);
+        if (r == 0)
+        {
+            rb.AddTorque(Random.Range(0, 1000) % 10, Random.Range(0, 1000) % 10, Random.Range(0, 1000) % 10);
+            rb.AddForce(Random.Range(0, 1000) % 10 * 20, Random.Range(0, 1000) % 10 * 20, Random.Range(0, 1000) % 10 * 20);
+        }
+        else
+        {
+            rb.AddForce(-Random.Range(0, 1000) % 10 * 20, -Random.Range(0, 1000) % 10 * 20, -Random.Range(0, 1000) % 10 * 20);
+            rb.AddTorque(-Random.Range(0, 1000) % 10, -Random.Range(0, 1000) % 10, -Random.Range(0, 1000) % 10);
+        }
+
+    }
 }
