db8abe30ccefb377970b8d4f54adf26479d6a8c6
Secil Kiroglu <secilkiroglu@gmail.com>
2017-12-28 16:10:22 +0300

    Improved Login screen

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -3,51 +3,114 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
 
 public class Login : MonoBehaviour {
 
     public Button yourButton;
-    // Use this for initialization
-    void Start () {
-        //GameObject hudcanvas = GameObject.FindGameObjectWithTag("HUDCanvas");
-        //GameObject logincanvas = GameObject.FindGameObjectWithTag("LoginCanvas");
+    public GameObject username;
+    public GameObject password;
+    private string Username;
+    private string Password;
+    private string[] Lines;
+    private string DecrptedPassword;
+    public Text t;
+
+
+    void Start()
+    {
         GameObject.Find("HUDCanvas").GetComponent<Canvas>().enabled = false;
     }
 
+    public void LoginButton() {
+        bool UN = false;
+        bool PW = false;
+        if (Username != "")
+        {
+            if (System.IO.File.Exists(@"./" + Username + ".txt"))
+            {
+                UN = true;
+                Lines = System.IO.File.ReadAllLines(@"./" + Username + ".txt");
+            }
+            else
+            {
+                t.text = ("Username incorrect!");
+            }
+        }
+        else {
+            t.text = ("Username field empty!");
+        }
+        if (Password != "")
+        {
+            if (System.IO.File.Exists(@"./" + Username + ".txt"))
+            {
+                int i = 1;
+                foreach (char c in Lines[2])
+                {
+                    i++;
+                    char Decrypted = (char)(c / i);
+                    DecrptedPassword += Decrypted.ToString();
+                }
+                if (Password == DecrptedPassword)
+                {
+                    PW = true;
+                }
+                else
+                {
+                    t.text = ("Password is invalid!");
+                }
+            }
+            else
+            {
+                t.text = ("User does not exist!");
+            }
+        }
+        else {
+            t.text = ("Username field empty!");
+        }
+        if (UN == true && PW == true) {
+            username.GetComponent<InputField>().text="";
+            password.GetComponent<InputField>().text="";
+            t.text = ("Login Successful!");
+            GameObject.Find("HUDCanvas").GetComponent<Canvas>().enabled = true;
+            Scene active = SceneManager.GetActiveScene();
+            SceneManager.LoadSceneAsync("scene1");
 
-    //yourButton.onClick.AddListener(TaskOnClick);
-
- 
-    //GameObject login = GameObject.FindGameObjectWithTag("LoginButton");
-
-    // Update is called once per frame 
-    void Update () {
-        //GameObject hudcanvas = GameObject.FindGameObjectWithTag("HUDCanvas");
-        //GameObject logincanvas = GameObject.FindGameObjectWithTag("LoginCanvas");
-        //GameObject login = GameObject.FindGameObjectWithTag("LoginButton");
-        //Destroy(transform.parent.gameObject);
-
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("scene1"));
+            SceneManager.UnloadSceneAsync(active);
+        }
     }
 
+    
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Password != "" && Username != "")
+            {
+                LoginButton();
+            }
+        }
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+    }
+
+    /*
     public void TaskOnClick()
     {
-        //Destroy(transform.parent.gameObject);
         GameObject.Find("HUDCanvas").GetComponent<Canvas>().enabled = true;
         Scene active = SceneManager.GetActiveScene();
         SceneManager.LoadSceneAsync("scene1");
         
         SceneManager.SetActiveScene(SceneManager.GetSceneByName("scene1"));
-        SceneManager.UnloadSceneAsync(active);
-        
-        
-        //SceneManager.LoadScene("scene1");//LoadLevel("scene1");
-    }
-
-    //private void OnMouseDown()
-    //{
-    //    //GameObject login = GameObject.FindGameObjectWithTag("LoginButton");
-    //    //Destroy(login);
-    //    Destroy(transform.parent.gameObject);
-    //}
+        SceneManager.UnloadSceneAsync(active);      
+    }*/
 
 }
---
+++ Assets/Scripts/register.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using UnityEngine.Collections;
+
+public class register : MonoBehaviour {
+	public GameObject username;
+	public GameObject password;
+	public GameObject email;
+	public GameObject confPassword;
+    public Text t;
+	private string Username;
+	private string Password;
+	private string form;
+	private string Email;
+	private string ConfPassword;
+	private bool EmailValid = false;
+	private string[] Characters = {"0","1","2","3","4","5","6","7","8","9","_","-","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","p","Q","R","S","T","U","V","W","X","Y","Z"};
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void RegisterButton() {
+		//Debug.Log("Registration successful!");
+
+		bool UN = false;
+		bool EM = false;
+		bool PW = false;
+		bool CPW = false;
+
+		if (Username != "") {
+			if (!System.IO.File.Exists(@"./" + Username + ".txt")) {
+				UN = true;
+			}
+			else
+			{
+				t.text = ("Username Taken!");             
+			}
+		}
+		else {
+            t.text = ("Username field empty!");
+		}
+		if (Email != "") {
+			EmailValidation();
+			if (EmailValid){
+				if (Email.Contains("@")){
+					if (Email.Contains(".")) {
+						EM = true;
+					}
+					else{
+                        t.text = ("Email is incorrect!");
+					}
+				}
+				else{
+                    t.text = ("Email is incorrect!");
+				}
+			}
+			else {
+                t.text = ("Email is incorrect!");
+			}
+		}
+		else {
+            t.text = ("Email field is empty!");
+		}
+		if (Password != "") {
+			if (Password.Length > 5) {
+				PW = true;
+			}
+			else{
+                t.text = ("Password must be at least 6 characters long!");
+			}
+		}
+		else {
+            t.text = ("Password field is empty!");
+		}
+		if (ConfPassword != "") {
+			if (ConfPassword == Password) {
+				CPW = true;
+			}
+			else{
+                t.text = ("Passwords do not match!");
+			}
+		}
+		else {
+            t.text = ("Confirm Password Field is empty!");
+		}
+		if(UN == true && PW == true && EM == true && CPW == true) {
+			bool Clear = true;
+			int i = 1;
+			foreach (char c in Password) {
+				if (Clear) {
+					Password = "";
+					Clear = false;
+				}
+				i++;
+				char Encrypted = (char)(c * i);
+				Password += Encrypted.ToString();
+			}
+			form = (Username + System.Environment.NewLine + Email + System.Environment.NewLine  + Password);
+			System.IO.File.WriteAllText(@"./" + Username + ".txt",form);
+			username.GetComponent<InputField>().text="";
+			password.GetComponent<InputField>().text="";
+			email.GetComponent<InputField>().text="";
+			confPassword.GetComponent<InputField>().text="";
+            t.text = ("Registration Complete!");
+		}
+	  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab)) {
+			if (username.GetComponent<InputField>().isFocused) {
+				email.GetComponent<InputField>().Select();
+			}
+			if (email.GetComponent<InputField>().isFocused)
+			{
+				password.GetComponent<InputField>().Select();
+			}
+			if (password.GetComponent<InputField>().isFocused)
+			{
+				confPassword.GetComponent<InputField>().Select();
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			if (Password != "" && Email != "" && Username != "" && ConfPassword != "") {
+				RegisterButton();
+			}
+		}
+
+		Username = username.GetComponent<InputField>().text;
+		Password = password.GetComponent<InputField>().text;
+		Email = email.GetComponent<InputField>().text;
+		ConfPassword = confPassword.GetComponent<InputField>().text;
+	}
+
+	void EmailValidation() {
+		bool SW = false;
+		bool EW = false;
+		for (int i = 0; i < Characters.Length; i++)
+		{
+			if (Email.StartsWith(Characters[i]))
+			{
+				SW = true;
+			}
+		}
+		for (int i = 0; i < Characters.Length; i++) {
+			if (Email.EndsWith(Characters[i]))
+			{
+				EW = true;
+			}
+		}
+		if (SW == true && EW == true)
+		{
+			EmailValid = true;
+		}
+		else {
+			EmailValid = false;
+		}
+	}
+
+}
